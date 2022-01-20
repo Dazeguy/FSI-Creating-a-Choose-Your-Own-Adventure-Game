65753ce693773426f49527dc972bc9db4ee29ec8
Shane <shane.yang@yahoo.com>
2022-01-19 20:56:13 -0500

    slight changes

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your Code Here
+window.prompt('Do you head left or right?')
+let answer  = window.prompt('Do you head left or right?')
+:left answer = window.prompt('Do you head left or right?')
+console.log(firstAnswer)
