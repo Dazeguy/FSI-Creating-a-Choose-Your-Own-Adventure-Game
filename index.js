a6943ebdce879404ab496c9d2e0bc777ca513d45
Shane <shane.yang@yahoo.com>
2022-01-22 16:48:26 -0500

    2nd part of secondAnswer

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 // Your Code Here
 let firstAnswer  = window.prompt('Do you head left or right?')
     if(firstAnswer === 'left'){
-    let secondAnswer = window.prompt(`You come across a stray cat. 
-    It scampers off down a small hole, just large enough for you to crawl through. 
-    Do you follow it, or continue on your path?`)
-        if(answer === follow){
-        let answer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.')
-            if(answer === stay){
-                let answer = window.prompt('You live happily amongst the cats for the rest of your days.')
-            }
-            else if(answer == Spread_the_word){
-                let answer = window.prompt('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.')
-            }
-        else if(answer === Continue){
-            let answer = window.promopt('You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?')
-        }
-            if
-    
-        
-        
-        } else if(firstAnswer === 'right')
+        let secondAnswer = window.prompt(`You come across a stray cat. 
+        It scampers off down a small hole, just large enough for you to crawl through. 
+        Do you follow it, or continue on your path?`)
+            if(secondAnswer === follow_it){
+                'You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.'
+            }   
+                let(follow_it)
+
+    }        else if(firstAnswer === 'right'){
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
 On the other side of him, you see a shiny chest of treasure. Another path would 
-lead you away from the dragon altogether. Which path do you take?`)}
+lead you away from the dragon altogether. Which path do you take?`)
+
+}
